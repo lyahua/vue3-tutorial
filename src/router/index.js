5676539fa85450187c4900731be97547bd05d40a
liuyahua <1251271810@qq.com>
2022-12-01 21:35:49 +0800

    feature:初始化

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,14 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/test",
+    name: "Test",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/day01/Test.vue"),
   },
 ];
 
---
+++ src/views/day01/Test.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        1111
+        <h2>姓名:{{ obj.name }}</h2>
+        <h2>年龄:{{ obj.age }}</h2>
+
+        <h2>姓名2:{{ name }}</h2>
+
+        <button @click="changeName">改变姓名</button>
+    </div>
+</template>
+
+<script>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, reactive, ref } from 'vue';
+export default {
+    setup() {
+        const obj = reactive({ name: 'zs', age: 18 })
+        const name = ref(obj.name)
+
+        const changeName = () => {
+            obj.name = 'ls'
+            name.value = '0000'
+        }
+
+        onBeforeMount(() => {
+            console.log('onBeforeMount')
+        })
+
+        onMounted(() => {
+            console.log('onMounted')
+        })
+
+        onBeforeUpdate(() => {
+            console.log('onBeforeUpdate')
+        })
+
+        onUpdated(() => {
+            console.log('onUpdated')
+        })
+
+        onBeforeUnmount(() => {
+            console.log('onBeforeUnmount')
+        })
+
+        onUnmounted(() => {
+            console.log('onUnmounted')
+        })
+
+        return { obj, name, changeName }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
